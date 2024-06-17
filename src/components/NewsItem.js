d6d827164ecd0242f3d857471209890f63338542
Subir-Droid <subirdeyabhi@gmail.com>
2024-06-17 21:42:19 +0530

    include image

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,15 +5,15 @@ export class NewsItem extends Component {
   static propTypes = {}
 
   render() {
-    let {title , description} = this.props;
+    let {title , description , imageUrl} = this.props;
     return (
         <div className="container my-3">
         <div className="card" style={{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href="/" className="btn btn-primary">Go somewhere</a>
+          <a href="/" className="btn btn-sn btn-primary">Read More ...</a>
         </div>
       </div>
       </div>
