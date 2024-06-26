cbd2b3c7c3bdf0bb9a92a8edcb30d7efed59c85d
Subir-Droid <subirdeyabhi@gmail.com>
2024-06-26 23:27:28 +0530

    add infiite loading

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
 
 export class Navbar extends Component {
   static propTypes = {}
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
     
@@ -27,41 +28,39 @@ async componentDidMount(){
 }
 
 async makeAPiCall(){
-    this.state.loading = true;
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=de6735f936e04444998a407a55684e9f&page=${this.state.page}&pagesize=10`;
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({articles : parsedData.articles , totalArticles : parsedData.totalResults , loading:false })
+    this.setState({articles : this.state.articles.concat(parsedData.articles)  , totalArticles : parsedData.totalResults })
 }
 
-handlePreviousClick = async () => {
-    this.setState({page : this.state.page - 1})
-    this.makeAPiCall();
-}
-
-handleNextClick = async () =>{
+fetchMore = () =>{
     this.setState({page : this.state.page + 1})
     this.makeAPiCall();
 }
 
   render() {
     return (
-        <div className='container my-3'>
+        <>
         <h2>Top {this.props.category}</h2>
-        {this.state.loading && <Spinner/>}
-        {!this.state.loading && <div className='row'>   
+        
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMore}
+          hasMore={this.state.articles.length !== this.state.totalArticles}
+          loader={<div><h1>Loading...</h1></div>}
+        >
+            <div className='container my-3'>
+            <div className='row'>   
             {this.state.articles?.map((element) =>{
                 return <div className='col-md-4' key={element.url}>
                 <NewsItem title={element.title?.slice(0,20)} description = {element.description?.slice(0,40)} imageUrl={!element.urlToImage ?"https://s.yimg.com/ny/api/res/1.2/APmNatbOT7Ud03AEXlUMsg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2024-06/6d2d9070-2b52-11ef-a7ff-07085b1db9a8" : element.urlToImage} newsUrl = {element.url}/>
                 </div>
             })}
-            
-        </div>}
-        <div className='container d-flex justify-content-around'>
-        <button disabled={this.state.page <= 1} type="button" className="btn btn-primary" onClick={this.handlePreviousClick}>Previous</button>
-        <button type="button" className="btn btn-primary" disabled={this.state.page > this.state.totalArticles / 10 }  onClick={this.handleNextClick}>Next</button>
         </div>
-        </ div>
+        </div>
+        </InfiniteScroll>
+        </>
     )
   }
 }
